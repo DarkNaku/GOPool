08cd330ec3f6517d7b79b6d99171a732d5bbe680
DarkNaku <skyrack@naver.com>
2024-12-18 16:57:51 +0900

    도메인 리로드 안하는 경우 오류 수정

--- Assets/GOPool/Runtime/GOPool.cs
+++ Assets/GOPool/Runtime/GOPool.cs
@@ -56,6 +56,7 @@ namespace DarkNaku.GOPool
         private static void OnSubsystemRegistration()
         {
             _instance = null;
+            _isDestroyed = false;
             _configRegistered = false;
         }
 
